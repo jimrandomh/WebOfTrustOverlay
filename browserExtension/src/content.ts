e57fcda47b23f3f07c95deeb8d86dfca7f821842
James Babcock <jimrandomh@gmail.com>
2024-03-03 11:25:45 -0800

    Add WoT badge support on Twitter

--- browserExtension/src/content.ts
+++ browserExtension/src/content.ts
@@ -2,9 +2,14 @@
  * Web of Trust Overlay content script
  */
 
+type TimeToRecheckForUsernames = "onLoad"|"onInterval";
+
 abstract class SiteConfig {
   abstract findUsernameElements(): Element[]
 
+  getTimesToRecheck(): TimeToRecheckForUsernames[] {
+    return ["onLoad"]
+  }
   getUsernameFromElement(el: Element): string{
     return el.textContent ?? "";
   }
@@ -15,16 +20,28 @@ const siteConfigs: Record<string,SiteConfig> = {
     findUsernameElements(): Element[] {
       return getElementsByClassName("hnuser");
     }
-  }
+  },
+  "twitter.com": new class extends SiteConfig {
+    findUsernameElements(): Element[] {
+      return filterNonnull(
+        getElementsByCssSelector('[data-testid="User-Name"]')
+          .map(element => element.children.item(element.children.length-2))
+      );
+    }
+    getTimesToRecheck(): TimeToRecheckForUsernames[] {
+      return ["onInterval"]
+    }
+  },
 }
 
 function createBadgeElement(username: string): Element {
   const state = { isOpen: false };
   const root = document.createElement("span");
+  root.setAttribute("class", "webOfTrustOverlayBadge");
   root.setAttribute("style", cssObjectToStyleAttribute({
     position: "relative",
     cursor: "pointer",
-    color: "black",
+    color: "#333",
   }));
   root.innerText = '[]';
   root.onclick = () => {
@@ -56,6 +73,10 @@ function createBadgeMenu(username: string): Element {
     padding: "6px",
   }));
   
+  const menuHeader = document.createElement("div");
+  menuHeader.innerText = username;
+  root.append(menuHeader);
+
   root.append(createMenuItem({
     label: "Vouch Human"
   }));
@@ -116,10 +137,28 @@ function insertElementAfter(element: Element, after: Element) {
 }
 
 function getElementsByClassName(cl: string): Element[] {
-  const elementCollection = document.getElementsByClassName(cl);
-  const result: Element[] = [];
-  for (let i=0; i<elementCollection.length; i++) {
-    const element = elementCollection.item(i)
+  return htmlCollectionToArray(document.getElementsByClassName(cl));
+}
+
+function getElementsByCssSelector(selector: string): Element[] {
+  return nodeListToArray(document.querySelectorAll(selector));
+}
+
+function htmlCollectionToArray<T extends Element>(elements: HTMLCollectionOf<T>): T[] {
+  const result: T[] = [];
+  for (let i=0; i<elements.length; i++) {
+    const element = elements.item(i)
+    if (element) {
+      result.push(element);
+    }
+  }
+  return result;
+}
+
+function nodeListToArray<T extends Node>(elements: NodeListOf<T>): T[] {
+  const result: T[] = [];
+  for (let i=0; i<elements.length; i++) {
+    const element = elements.item(i)
     if (element) {
       result.push(element);
     }
@@ -135,21 +174,72 @@ function cssObjectToStyleAttribute(obj: Record<string,string>) {
   return sb.join("");
 }
 
+function filterNonnull<T>(arr: Array<T|null>): Array<T> {
+  const result: T[] = [];
+  for (let element of arr) {
+    if (element) {
+      result.push(element)
+    }
+  }
+  return result;
+}
+
+function elementHasClass(el: Element, className: string): boolean {
+  const classAttr = el.getAttribute("class");
+  if (!classAttr) return false;
+  return classAttr.split(" ").some(c => c===className);
+}
+
+
+function getSiteConfig(): SiteConfig|null {
+  const domain = window.location.hostname;
+  const siteConfig = siteConfigs[domain];
+  return siteConfig ?? null;
+}
 
 function addBadgeAfter(element: Element, username: string) {
   const badge = createBadgeElement(username);
   insertElementAfter(badge, element);
 }
 
-function contentScriptMain() {
-  const domain = window.location.hostname;
-  const siteConfig = siteConfigs[domain];
+function elementAlreadyHasOverlayBadge(element: Element) {
+  const parentElement = element.parentElement;
+  if (!parentElement) return false;
+  for (const sibling of htmlCollectionToArray(parentElement.children)) {
+    if (elementHasClass(sibling, "webOfTrustOverlayBadge"))
+      return true;
+  }
+  return false;
+}
+
+function checkForUsernameElements() {
+  const siteConfig = getSiteConfig();
   if (!siteConfig) return;
   
   const elements = siteConfig.findUsernameElements();
   for (let element of elements) {
+    if (elementAlreadyHasOverlayBadge(element))
+      continue;
     const username = siteConfig.getUsernameFromElement(element);
     addBadgeAfter(element, username);
+  }
+}
+function contentScriptMain() {
+  const siteConfig = getSiteConfig();
+  if (siteConfig) {
+    console.log(`Loading Web of Trust content script`);
+  } else {
+    return;
+  }
+
+  if(siteConfig.getTimesToRecheck().find(t=>t==="onLoad")) {
+    checkForUsernameElements();
+  }
+  
+  if(siteConfig.getTimesToRecheck().find(t=>t==="onInterval")) {
+    setInterval(() => {
+      checkForUsernameElements();
+    }, 200);
   }
 }
 
